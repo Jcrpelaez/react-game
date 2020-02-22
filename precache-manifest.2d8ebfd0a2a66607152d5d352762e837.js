639370f4fac1948b63159e0618d32647dbb6d0e3
Juan Carlos <jcrpelaez@yahoo.com>
2020-02-22 15:37:37 -0600

    Updates

---
+++ precache-manifest.2d8ebfd0a2a66607152d5d352762e837.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cad85c5dec27e4395a2f0bed5aa63ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00b3d22840c1a91c0aef",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "05e9b4c133790f06292c",
+    "url": "/static/js/2.73a5090f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.73a5090f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00b3d22840c1a91c0aef",
+    "url": "/static/js/main.80c897b9.chunk.js"
+  },
+  {
+    "revision": "cad452610cce2892fd69",
+    "url": "/static/js/runtime-main.970fcd28.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
